7ef1f4694c56e8c39a9d214ca0726b7e5ddac77d
solonring <solon.ring2011@gmail.com>
2019-07-16 16:29:44 +0800

    update

--- application/config/config_exp.php
+++ application/config/config_exp.php
@@ -3,8 +3,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['default_set_register'] = array('3'=>'游客');
 $config['default_set'] = array('1'=>'管理员','2'=>'开发','3'=>'游客');
-$config['type']	=	array('1'=>'爱个购-devshow.aigegou.com','2'=>'o2o-o2odev.aigegou.com','3'=>'跑腿帮-ptbdev.aigegou.com','4'=>'商城-malldev.aigegou.com');
-$config['go_url']	=	array('1'=>'http://www.aigegou.cc/mobile/index.php?',
-							'2'=>'http://o2odev.aigegou.com/mobile/index.php?',
-							'3'=>'http://ptbdev.aigegou.com/mobile/index.php?',
-							'4'=>'http://malldev.aigegou.com/mobile/index.php?');
+$config['type']	=	array('1'=>'API-www.interface.cc');
+$config['go_url']	=	array('1'=>'http://www.interface.cc/index.php?');
