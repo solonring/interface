b996d5553fe2859781075a4d517b4d7de00175ce
solonring <solon.ring2011@gmail.com>
2019-07-30 15:50:23 +0800

    vscode reset

--- application/config/config_exp.php
+++ application/config/config_exp.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['default_set_register'] = array('3'=>'游客1');
+$config['default_set_register'] = array('3'=>'游客');
 $config['default_set'] = array('1'=>'管理员','2'=>'开发','3'=>'游客');
 $config['type']	=	array('1'=>'API-www.interface.cc');
 $config['go_url']	=	array('1'=>'http://www.interface.cc/index.php?');
